dcc478633333df01d915c15e9b6761ee2e16e75b
Edgardo David <edgardo.burgos94@gmail.com>
2017-11-15 22:35:07 -0500

    Suponer que params puede ser nil

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -3,18 +3,19 @@ class ExpensesController < ApplicationController
    #@expenses = Expense.order("date DESC")
    concept = params[:concept]
    category_id = params[:category_id]
-   # puts concept.length
-   # puts category_id.length
-   if category_id.length == 0 && concept.length > 0
+   puts concept == nil
+   puts category_id.class
+
+   if category_id.class == nil && concept.class != nil
      expense = Expense.where(concept: params[:concept])
      @expenses = expense
-   elsif category_id.length > 0 && concept.length == 0
+   elsif category_id.class != nil && concept.class == nil
      expense = Category.find(category_id).expenses
      @expenses = expense
-   elsif category_id.length > 0 && concept.length > 0
+   elsif category_id.class != nil && concept.class != nil
      expense = Category.find(params[:category_id]).expenses.where(concept: params[:concept])
      @expenses = expense
-   else category_id.length == 0 && concept.length == 0
+   else category_id.class == nil && concept.class == nil
      @expenses = Expense.order("date DESC")
    end
    # puts @expenses
