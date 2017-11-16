3801c0fdb955be0c5f4832e3c9cb680de02bb2d8
Edgardo David <edgardo.burgos94@gmail.com>
2017-11-15 22:50:14 -0500

    Si es nil lo vuelve string

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,24 +1,30 @@
 class ExpensesController < ApplicationController
   def index
-   #@expenses = Expense.order("date DESC")
-   concept = params[:concept]
-   category_id = params[:category_id]
-   # puts concept == nil
-   # puts category_id.class
+    #@expenses = Expense.order("date DESC")
+        category_id = params[:category_id]
+        concept = params[:concept]
+        if category_id == nil
+          category_id = ""
+        end
+        if concept == nil
+          concept = ""
+        end
 
-   if category_id.class == nil && concept.class != nil
-     expense = Expense.where(concept: params[:concept])
-     @expenses = expense
-   elsif category_id.class != nil && concept.class == nil
-     expense = Category.find(params[:category_id]).expenses
-     @expenses = expense
-   elsif category_id.class != nil && concept.class != nil
-     expense = Category.find(params[:category_id]).expenses.where(concept: params[:concept])
-     @expenses = expense
-   else category_id.class == nil && concept.class == nil
-     @expenses = Expense.order("date DESC")
-   end
-   # puts @expenses
-   # puts "-----> LA VARIABLE DE INSTANCIA ES #{@expenses.class}"
+        # puts concept.length
+        # puts category_id.length
+        if category_id.length == 0 && concept.length > 0
+          expense = Expense.where(concept: concept)
+          @expenses = expense
+        elsif category_id.length > 0 && concept.length == 0
+          expense = Category.find(category_id).expenses
+        @expenses = expense
+        elsif category_id.length > 0 && concept.length > 0
+          expense = Category.find(category_id).expenses.where(concept: concept)
+          @expenses = expense
+        else category_id.length == 0 && concept.length == 0
+           @expenses = Expense.order("date DESC")
+        end
+        # puts @expenses
+        # puts "-----> LA VARIABLE DE INSTANCIA ES #{@expenses.class}"
   end
 end
