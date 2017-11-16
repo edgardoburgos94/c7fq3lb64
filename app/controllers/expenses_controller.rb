eef608924df91e627f30d0e5a22727d25b4e590d
Edgardo David <edgardo.burgos94@gmail.com>
2017-11-15 22:17:42 -0500

    Agregarbusqueda

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,23 @@
 class ExpensesController < ApplicationController
   def index
+   #@expenses = Expense.order("date DESC")
+   concept = params[:concept]
+   category_id = params[:category_id]
+   # puts concept.length
+   # puts category_id.length
+   if category_id.length == 0 && concept.length > 0
+     expense = Expense.where(concept: params[:concept])
+     @expenses = expense
+   elsif category_id.length > 0 && concept.length == 0
+     expense = Category.find(category_id).expenses
+     @expenses = expense
+   elsif category_id.length > 0 && concept.length > 0
+     expense = Category.find(params[:category_id]).expenses.where(concept: params[:concept])
+     @expenses = expense
+   else category_id.length == 0 && concept.length == 0
      @expenses = Expense.order("date DESC")
+   end
+   # puts @expenses
+   # puts "-----> LA VARIABLE DE INSTANCIA ES #{@expenses.class}"
   end
 end
