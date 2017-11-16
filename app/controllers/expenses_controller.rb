af7d312e30526f4a10a224858939d024d21de90e
Edgardo David <edgardo.burgos94@gmail.com>
2017-11-15 22:41:19 -0500

    Poner params dentro de la busqueda

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -3,14 +3,14 @@ class ExpensesController < ApplicationController
    #@expenses = Expense.order("date DESC")
    concept = params[:concept]
    category_id = params[:category_id]
-   puts concept == nil
-   puts category_id.class
+   # puts concept == nil
+   # puts category_id.class
 
    if category_id.class == nil && concept.class != nil
      expense = Expense.where(concept: params[:concept])
      @expenses = expense
    elsif category_id.class != nil && concept.class == nil
-     expense = Category.find(category_id).expenses
+     expense = Category.find(params[:category_id]).expenses
      @expenses = expense
    elsif category_id.class != nil && concept.class != nil
      expense = Category.find(params[:category_id]).expenses.where(concept: params[:concept])
